bfd310564ce09ff396a08e40ab108d0cbbda5921
Bojan Bedrac <bojan@bele-serv.com>
2015-03-24 23:43:30 +0100

    Alternate path

--- src/DogDoorSimulator.java
+++ src/DogDoorSimulator.java
@@ -7,7 +7,19 @@ public class DogDoorSimulator {
     System.out.println("Fido barks to go outside...");
     remote.pressButton();
 
+    System.out.println("\nFido goes outside.");
     System.out.println("\nFido's all done...");
+
+    try {
+        Thread.sleep(10000);
+    }
+    catch (InterruptedException e) {
+      System.out.println("Something went wrong :)");
+    }
+
+    System.out.println("\nFido scratches the door from the outside and barks");
+    System.out.println("\nTodd presses the remotes button.");
+
     remote.pressButton();
 
     System.out.println("\nFido's back inside...");
